8d6f968f456dadfffb3638fa311d30ae3ad36149
naohito-T <naohito.tanaka0523@gmail.com>
2021-09-22 17:23:11 +0900

    完成

--- src/util/carousel.ts
+++ src/util/carousel.ts
@@ -1,54 +1,70 @@
+// import { setupNavigation } from './navigation';
+
 export const carousel = (root: any) => {
-  let figure = root.querySelector('figure'),
+  /** figureを取り出す */
+  const figure = root.querySelector('figure'),
+    /** nav(button)を取り出す */
     nav = root.querySelector('nav'),
+    /** figureの子要素(imageを取得)  */
     images = figure?.children,
-    n = images?.length ?? 0,
+    /** figureの子要素のimageの個数を取得 */
+    n: number = images?.length ?? 0,
+    /** data-gapを取り出す(読み取り書き込みで) */
     gap = root.dataset.gap || 0,
+    /** data-gapにdata-bfcがsetされているか? true */
     bfc = 'bfc' in root.dataset,
+    /**  */
     theta = (2 * Math.PI) / n,
-    currImage = 0;
-  setupCarousel(n, parseFloat(getComputedStyle(images[0]).width));
+    /** imageのindex */
+    currentImage = 0;
+
   window.addEventListener('resize', () => {
     setupCarousel(n, parseFloat(getComputedStyle(images[0]).width));
   });
 
-  setupNavigation();
-
-  function setupCarousel(n: any, s: any) {
-    var apothem = s / (2 * Math.tan(Math.PI / n));
+  const setupCarousel = (n: number, s: number) => {
+    const apothem = s / (2 * Math.tan(Math.PI / n));
     figure.style.transformOrigin = `50% 50% ${-apothem}px`;
 
     for (var i = 0; i < n; i++) images[i].style.padding = `${gap}px`;
+
     for (i = 1; i < n; i++) {
       images[i].style.transformOrigin = `50% 50% ${-apothem}px`;
       images[i].style.transform = `rotateY(${i * theta}rad)`;
     }
+
     if (bfc)
       for (i = 0; i < n; i++) images[i].style.backfaceVisibility = 'hidden';
 
-    rotateCarousel(currImage);
-  }
+    rotateCarousel(currentImage);
+  };
 
-  function setupNavigation() {
+  /**
+   * @desc carouselを回す
+   * @param imageIndex
+   */
+  const rotateCarousel = (imageIndex: number) => {
+    figure.style.transform = `rotateY(${imageIndex * -theta}rad)`;
+  };
+
+  const setupNavigation = (nav: any, imgIndex: number) => {
     nav.addEventListener('click', onClick, true);
 
     function onClick(e: any) {
       e.stopPropagation();
 
-      var t = e.target;
+      const t = e.target;
       if (t.tagName.toUpperCase() != 'BUTTON') return;
 
       if (t.classList.contains('next')) {
-        currImage++;
+        imgIndex++;
       } else {
-        currImage--;
+        imgIndex--;
       }
 
-      rotateCarousel(currImage);
+      rotateCarousel(imgIndex);
     }
-  }
-
-  function rotateCarousel(imageIndex: any) {
-    figure.style.transform = `rotateY(${imageIndex * -theta}rad)`;
-  }
+  };
+  setupNavigation(nav, currentImage);
+  setupCarousel(n, parseFloat(getComputedStyle(images[0]).width));
 };
